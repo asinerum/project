69f74637a8bd6a9e48b29f32332197fb54527ee0
Asinerum Conlang Project <46498542+asinerum@users.noreply.github.com>
2020-01-09 00:59:38 +0700

    V8 Xuteng Client Control

---
+++ require.js
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////
+const require=function(js){document.write('<script\tsrc="'+js+'"></script>');};
+////////////////////////////////////////////////////////////
+require('scr/abis.js');
+require('scr/babies.js');
+require('scr/base.js');
+require('scr/consts.js');
+require('scr/consts-author.js');
+require('scr/dconsts.js');
+require('scr/dconsts-author.js');
+require('scr/ethercall.js');
+require('scr/ethercall-author.js');
+require('scr/ethersend.js');
+require('scr/forms.js');
+require('scr/forms-author.js');
+require('scr/gaps.js');
+require('scr/gui.js');
+require('scr/gui-author.js');
+require('scr/guidat.js');
+require('scr/guidat-author.js');
+require('scr/hi.js');
+require('scr/hi-author.js');
+require('scr/hx.js');
+require('scr/index.js');
+////////////////////////////////////////////////////////////
