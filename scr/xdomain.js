bb828fc7b66e835c36befdf635821d7913502459
Antonio Hoang <antonius.hoang@gmail.com>
2020-05-25 08:27:51 +0700

    Edit Xdomain

--- scr/xdomain.js
+++ scr/xdomain.js
@@ -5,7 +5,7 @@ quanbua:'0x94eB08be475F9E8b1458C5FEA1EE786c3b4799d9',
 gamer:'0xF4CCF0cC09EeA48E29A0D9B84f285bb2E601FeFe',
 };
 ////////////////////////////////////////////////////////////
-const DOMAINS={
+const DOMAIN={
 asinero:'asinero',
 asinerum:'asinerum',
 gamer:'gamer',
