b37724e9c85dff77cf4596bbdb76c71b2be20d5b
Antonio Hoang <antonius.hoang@gmail.com>
2020-06-02 09:28:46 +0700

    Reupload Consts

---
+++ scr/consts-author.web.js
@@ -0,0 +1,71 @@
+////////////////////////////////////////////////////////////BUYER
+const _buyPostNum='_buyPostNum';
+const _buyDone='_buyDone';
+const _buyTpe='_buyTpe';
+const _buyVol='_buyVol';
+const _buyNum='_buyNum';
+////////////////////////////////////////////////////////////SELLER
+const _sellPostNum='_sellPostNum';
+const _sellDone='_sellDone';
+const _sellTpe='_sellTpe';
+const _sellVol='_sellVol';
+const _sellNum='_sellNum';
+////////////////////////////////////////////////////////////BUYING
+const _sellerAddress='sellerAddress';
+const _sellerPostNum='sellerPostNum';
+const _sellingDone='_sellingDone';//traded
+const _sellingTpe='_sellingTpe';//price
+const _sellingVol='_sellingVol';//trading
+const _sellingNum='_sellingNum';//count
+const _xut2gain='xut2gain';
+const _eth2send='eth2send';
+////////////////////////////////////////////////////////////SELLING
+const _buyerAddress='buyerAddress';
+const _buyerPostNum='buyerPostNum';
+const _buyingDone='_buyingDone';//traded
+const _buyingTpe='_buyingTpe';//price
+const _buyingVol='_buyingVol';//trading
+const _buyingNum='_buyingNum';//count
+const _xut2send='xut2send';
+const _eth2gain='eth2gain';
+////////////////////////////////////////////////////////////BUY
+const _buyrate='buyrate';
+const _xut2buy='xut2buy';
+const _eth2pay='eth2pay';
+////////////////////////////////////////////////////////////SELL
+const _sellrate='sellrate';
+const _xut2sell='xut2sell';
+const _eth2recv='eth2recv';
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////USER
+const _userName='userName';
+const _userForm='userForm';
+const _userText='userText';
+////////////////////////////////////////////////////////////DOCS
+const _inBlock='inBlock';
+const _inHash='inHash';
+const _inTyp='inType';
+const _inFmt='inForm';
+const _inTxt='inText';
+const _inTag='inTags';
+const _inHed='inHead';
+const _inFut='inFoot';
+const _inAut='inAuth';
+const _inRef='inRef';
+const _inUtc='inUtc';
+const _inFrm='inFrm';
+const _inTo='inTo';
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////MSGS
+const _keystore='keystore';
+const _smessage='message';
+const _accverify='verify';
+const _signature='signature';
+const _signatory='signatory';
+////////////////////////////////////////////////////////////ACCS
+const _newaddress='new_address';
+const _newprivate='new_private';
+const _oldaddress='old_address';
+const _oldprivate='old_private';
+const _expprivate='exp_private';
+////////////////////////////////////////////////////////////
