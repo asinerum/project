110c1165a589ddb687a6ae9bef43c75849f78ca7
Asinerum Conlang Project <46498542+asinerum@users.noreply.github.com>
2020-07-09 10:28:41 +0700

    Edit xuteng.user.js

--- xuteng.user.js
+++ xuteng.user.js
@@ -3,6 +3,7 @@ const restyle=function(ss){document.write('<link\thref="'+ss+'"\trel="stylesheet
 const require=function(js){document.write('<script\tsrc="'+js+'"></script>');};
 ////////////////////////////////////////////////////////////
 restyle('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+restyle('https://cdn.jsdelivr.net/npm/trumbowyg@2.21.0/dist/ui/trumbowyg.min.css');
 restyle('https://asinerum.github.io/project/gui/fonts.css');
 restyle('https://asinerum.github.io/project/gui/style.css');
 ////////////////////////////////////////////////////////////
@@ -10,6 +11,7 @@ require('https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js');
 require('https://ajax.googleapis.com/ajax/libs/angularjs/1.7.6/angular.min.js');
 require('https://cdn.jsdelivr.net/gh/ethereum/web3.js@1.0.0-beta.34/dist/web3.min.js');
 require('https://cdn.jsdelivr.net/gh/ethereumjs/browser-builds/dist/ethereumjs-tx/ethereumjs-tx-1.3.3.min.js');
+require('https://cdn.jsdelivr.net/npm/trumbowyg@2.21.0/dist/trumbowyg.min.js');
 require('./scr/xuteng13.js');
 require('./scr/xutmsg13.js');
 require('./scr/xbutton.js');
