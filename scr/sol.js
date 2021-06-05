637d7927b22a4bf23b232875df75d63f8a6dd4c8
Asinerum Conlang Project <46498542+asinerum@users.noreply.github.com>
2021-06-06 02:07:29 +0700

    Update Sol.js and Test.js

--- scr/sol.js
+++ scr/sol.js
@@ -8,3 +8,6 @@ const solAirDrop=async(addr,sol)=>{if(!window.solana)solConnect();return(await(w
 const solBalance=async(addr)=>{if(!window.solana)solConnect();return(await(window.solana.getBalance(new solanaWeb3.PublicKey(addr))))};
 const solTransfer=async(kp,addr,sol,tx=null)=>{if(!window.solana)solConnect();tx=new solanaWeb3.Transaction().add(solanaWeb3.SystemProgram.transfer({fromPubkey:kp.publicKey,toPubkey:new solanaWeb3.PublicKey(addr),lamports:sol*1000000000}));return(await(solanaWeb3.sendAndConfirmTransaction(window.solana,tx,[kp])))};
 ////////////////////////////////////////////////////////////
+const solEncrypt=function(pw,key,cbf=console.log,res=null){bipEncrypt(pw,key.slice(0,64),function(e,r){if(e)return(cbf(e,null));res=r.bip;bipEncrypt(pw,key.slice(64,128),function(e,r){cbf(e,res+r.bip)})})};
+const solDecrypt=function(pw,bip,cbf=console.log,res=null){bipDecrypt(pw,bip.slice(0,58),function(e,r){if(e)return(cbf(e,null));res=r.hex;bipDecrypt(pw,bip.slice(58,116),function(e,r){cbf(e,res+r.hex)})})};
+////////////////////////////////////////////////////////////
