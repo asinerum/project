962ec441b3bb943141f17a9ce952d3ee5b8044ac
A. Roch <85383162+AntonRoch@users.noreply.github.com>
2021-10-22 01:20:40 +0700

    Add IPP defi libs

---
+++ scr/hipp.js
@@ -0,0 +1,48 @@
+﻿////////////////////////////////////////////////////////////
+const PROGMINRATE = 3;
+const PROGMAXRATE = 200;
+////////////////////////////////////////////////////////////
+const MBALANCE = 'balanceOf';
+const MPROGGET = 'programs';
+const MPROGINV = 'invests';
+const MPROGPAY = 'invest';
+const MPROGRAM = 'program';
+const MPROGSTP = 'close';
+const MPROGWDR = 'withdraw';
+const YEARSECONDS = 365*24*60*60;
+////////////////////////////////////////////////////////////
+let _days = 'days';
+let _martMoney = 'NEMT7';
+let _progMoney = 'GEMT7';
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+let _errClear = `"RedeemNotAllowed"`;
+let _errDepos = `"TokensNotEnough"`;
+let _errIdNot = `"IdNotAvailable"`;
+let _errInput = `"InputNotValid"`;
+let _errInvst = `"InvestNotAllowed"`;
+let _errItNot = `"ItemDoesNotExist"`;
+let _errOwner = `"ActionNotAllowed"`;
+let _errValue = `"ItemHasNoValue"`;
+////////////////////////////////////////////////////////////
+let _warnPrgDraw = `You are about to half redeem this one`;
+let _warnPrgIdno = `Program ID must be a positive integer`;
+let _warnPrgInit = `Deposit amounts must be larger than 0`;
+let _warnPrgNOwn = `You are not the owner of this program`;
+let _warnPrgRate = `APR in ${PROGMINRATE}-${PROGMAXRATE}%`;
+let _warnPrgStop = `You are deciding to stop this program`;
+////////////////////////////////////////////////////////////
+LABELS.en._button_DefiProgDraw = `HALF REDEEM`;
+LABELS.en._button_DefiProgGain = `REDEEM`;
+LABELS.en._button_DefiProgJoin = `INVEST`;
+LABELS.en._button_DefiProgOpen = `CREATE PROGRAM`;
+LABELS.en._button_DefiProgStop = `STOP PROGRAM`;
+LABELS.en._label_DefiProgAge = `Age`;
+LABELS.en._label_DefiProgAgi = `My Investment Age`;
+LABELS.en._label_DefiProgAmt = `My Invested Sum`;
+LABELS.en._label_DefiProgAPR = `APR {%}`;
+LABELS.en._label_DefiProgOwn = `Owner`;
+LABELS.en._label_DefiProgSum = `Balance`;
+LABELS.en._label_investor = `INVESTOR'S PANEL`;
+LABELS.en._label_programer = `PROGRAMER'S PANEL`;
+////////////////////////////////////////////////////////////
---
+++ scr/sipp.js
@@ -0,0 +1,51 @@
+﻿////////////////////////////////////////////////////////////
+const defiProgGain=function(status,divId,i){
+showLoad(status);i=Number(gv(divId));if(!window.investor||window.investor.refno!=i)return(dw(status,_errInput));if(window.investor.amount<=0||window.investor.value<=0)return(dw(status,_errClear));
+ercSend(xutengFemt,MPROGWDR,[i],0,status,null,function(err,res){checkResult(err,res,status);console.warn('WITHDRAWAL_RECEIPT',res);});};
+////////////////////////////////////////////////////////////
+const defiProgJoin=function(status,divAmount,a){
+showLoad(status);a=s2n(gv(divAmount));if(!positiveNum(a)||!window.investor)return(dw(status,_errInput));if(window.investor.start!=0||window.investor.value<=0)return(dw(status,_errInvst));
+ercCall(xutengFemt,MBALANCE,[sender],status,null,function(err,res){checkResult(err,res,status);console.warn('INVESTOR_BALANCE',w2s(res,9));if(fromWei(res)<a)return(dw(status,_errDepos));
+ercSend(xutengFemt,MPROGPAY,[window.investor.refno,s2w(a)],0,status,null,function(err,res){checkResult(err,res,status);console.warn('INVESTMENT_RECEIPT',res);});});};
+////////////////////////////////////////////////////////////
+const defiProgRead=function(status,divId,divRate,divAmount,divAge,divOwner,divInvest,divInAge,dec=5,i){
+showLoad(status);i=Number(gv(divId));if(!positiveInt(i))return(dw(status,_errInput));window.investor={};window.investor.refno=i;
+ercCall(xutengFemt,MPROGGET,[i],status,null,function(err,res){checkResult(err,res,status);db(divRate,bPetriToApr(res.petri));db(divAmount,w2s(res.value,dec));db(divAge,showItemAge(res.open));db(divOwner,showAddrUrl(res.maker));window.investor.value=res.value;console.warn('PROGRAM_DATA',res);
+ercCall(xutengFemt,MPROGINV,[i,sender],status,null,function(err,res){checkResult(err,res,status);db(divInvest,w2s(res.amount));db(divInAge,showItemAge(res.start));window.investor.amount=res.amount;window.investor.start=res.start;console.warn('INVEST_DATA',res);});});};
+////////////////////////////////////////////////////////////
+const defiProgStop=function(status,divId,half=true,i){
+showLoad(status);i=Number(gv(divId));if(!positiveInt(i))return(dw(status,_errInput));
+ercCall(xutengFemt,MPROGGET,[i],status,null,function(err,res){checkResult(err,res,status);console.warn('PROGRAM_DATA',res);if(res.maker==ZEROADDR)return(dw(status,_errItNot));if(!twoHexEqual(res.maker,sender))return(dw(status,_errOwner));if(res.value===ZERO)return(dw(status,_errValue));
+ercSend(xutengFemt,MPROGSTP,[i,half],0,status,null,function(err,res){checkResult(err,res,status);console.warn('PROGRAM_STOP_RECEIPT',res);});});};
+////////////////////////////////////////////////////////////
+const defiProgOpen=function(status,divId,divRate,divAmount,eth=0,i,r,a){
+showLoad(status);i=Number(gv(divId));r=s2n(gv(divRate));a=s2n(gv(divAmount));if(!positiveInt(i)||!positiveNum(a)||!numsInRange(r,PROGMINRATE,PROGMAXRATE))return(dw(status,_errInput));
+ercCall(xutengFemt,MPROGGET,[i],status,null,function(err,res){checkResult(err,res,status);console.warn('PROGRAM_DATA',res);if(res.maker!=ZEROADDR)return(dw(status,_errIdNot));
+ercCall(xutengFemt,MBALANCE,[sender],status,null,function(err,res){checkResult(err,res,status);console.warn('PROGRAMER_BALANCE',w2s(res,9));if(fromWei(res)<a)return(dw(status,_errDepos));
+ercSend(xutengFemt,MPROGRAM,[i,bAprToPetri(r),s2w(a)],s2w(eth),status,null,function(err,res){checkResult(err,res,status);console.warn('PROGRAM_CREATION_RECEIPT',res);});});});};
+////////////////////////////////////////////////////////////
+const bAprToPetri=function(apr){return(Math.round(10**9*apr/100/YEARSECONDS));};
+const bPetriToApr=function(spr){return(YEARSECONDS*100*spr/10**9);};
+const checkResult=function(err,res,status){if(err){showError(status);throw(err.toString())};if(!res){showCancel(status);throw(ERROR)};};
+const positiveInt=function(n){return(Number.isInteger(Number(n))&&n>0);};
+const positiveNum=function(n){return(Number(n)&&n>0);};
+const numsInRange=function(n,rl,rh){return(n>rl&&n<rh);};
+const showAddress=function(addr){if(addr==ZEROADDR)return('N/A');return(addr.toLowerCase());};
+const showAddrUrl=function(addr){return('<a target="_blank" href="'+EXCHAINS[network].scan+'/address/'+addr+'">'+showAddress(addr)+'</a>');};
+const showItemAge=function(stamp,say=_days){if(stamp==0)return('N/A');return(Math.floor((nowDate()-stamp)/(3600*24))+SPACE+say);};
+const twoHexEqual=function(h1,h2){return(fromHex(h1)===fromHex(h2));};
+////////////////////////////////////////////////////////////
+window.menu.onDefiProgDraw=function(){if(positiveInt(gv('pro_id'))&&confirm(_warnPrgDraw))return(defiProgStop('form_status','pro_id',true));}
+window.menu.onDefiProgGain=function(){defiProgGain('form_status','pro_id');}
+window.menu.onDefiProgJoin=function(){defiProgJoin('form_status','pro_gemt');}
+window.menu.onDefiProgOpen=function(){defiProgOpen('form_status','pro_id','pro_apr','pro_gemt');}
+window.menu.onDefiProgRead=function(){defiProgRead('form_status','pro_id','pro_apr','pro_gemt');}
+window.menu.onDefiProgReId=function(){defiProgRead('form_status','pro_id','_pro_apr','_pro_sum','_pro_age','_pro_own','_pro_amt','_pro_agi');}
+window.menu.onDefiProgStop=function(){if(positiveInt(gv('pro_id'))&&confirm(_warnPrgStop))return(defiProgStop('form_status','pro_id',false));}
+////////////////////////////////////////////////////////////
+window.menu.goDefiProgIdno=function(){if(!positiveInt(gv('pro_id')))return(alert(_warnPrgIdno));}
+window.menu.goDefiProgInit=function(){if(!positiveNum(s2n(gv('pro_gemt'))))return(alert(_warnPrgInit));}
+window.menu.goDefiProgRate=function(){if(!numsInRange(s2n(gv('pro_apr')),PROGMINRATE,PROGMAXRATE))return(alert(_warnPrgRate));}
+window.menu.goDefiProgInst=window.menu.goDefiProgInit;
+window.menu.goDefiProgReId=window.menu.onDefiProgReId;
+////////////////////////////////////////////////////////////
