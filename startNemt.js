1b1a1387324b7e0219f0d5ad7c080d23b80bd19c
A. Roch <85383162+AntonRoch@users.noreply.github.com>
2021-10-27 00:47:39 +0700

    Add files via upload

--- startNemt.js
+++ startNemt.js
@@ -1,4 +1,4 @@
 ////////////////////////////////////////////////////////////
-getMetamask(function(){startNemt();promoteMenu();dbh('pro_id','id',menu.onDefiProgReId)});
+getMetamask(function(){startNemt();dbh('pro_id','id',menu.onDefiProgReId)});
 txgwei=0;
 ////////////////////////////////////////////////////////////
