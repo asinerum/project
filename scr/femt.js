0c0306033ac48e1ed73d8e587055dd9d196fdefe
A. Roch <85383162+AntonRoch@users.noreply.github.com>
2023-07-29 03:52:32 +0700

    Shorten web3 functions

--- scr/femt.js
+++ scr/femt.js
@@ -4,7 +4,7 @@ const tokenAllowed=function(token){return([_progMoney,_martMoney,_rareMoney].inc
 const selectTokens=function(token){if(token==_progMoney)return(startGemt());if(token==_martMoney)return(startNemt());if(token==_rareMoney)return(startRemt())};
 const getLogApiUrl=function(block,blocks=1000,topic=MineLogTopic,sc=xutengFemt){return(PROXIES[0].getContractPastLogs(sc._address,block-blocks,block,topic))};
 const getLastMined=function(cbf=console.log,LP='lastProof',RS='rewardStamp'){try{if(Func(LP))ercFuncCall(LP).then(r=>{return(cbf(null,r))});else{if(Func(RS))ercFuncCall(RS).then(r=>{return(cbf(null,r))});else{nsBlock().then(r=>{$.getJSON(getLogApiUrl(r)).then(r=>{r=r.result;return(cbf(null,fromHex(r[r.length-1].timeStamp)))})})}}}catch(e){cbf(ERROR,null)}};
-const getTransLogs=function(cbf=console.log,blocks=1000,topic=MineLogTopic,sc=xutengFemt){web3.eth.getBlockNumber().then(block=>{$.getJSON(PROXIES[0].getContractPastLogs(sc._address,block-blocks,block,topic),function(data){if(data.status!='1')return(cbf(UNKNOWN,null));cbf(null,data.result)})})};
+const getTransLogs=function(cbf=console.log,blocks=1000,topic=MineLogTopic,sc=xutengFemt){nsBlock().then(block=>{$.getJSON(getLogApiUrl(block),function(data){if(data.status!='1')return(cbf(UNKNOWN,null));cbf(null,data.result)})})};
 const getPastMines=function(cbf=console.log,blocks=1000){return(getTransLogs(cbf,blocks))};
 ////////////////////////////////////////////////////////////
 const defiDigNonce=function(divNonce,pops=15,kc=kek,pf='basicRate'){showLoad(divNonce);nonce(pops,kc,pf,function(err,res){checkResult(err,res,divNonce);db(divNonce,res)})};
