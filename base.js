dd72a2a8a6d3f92517046aa858035fa784111e85
Asinerum Conlang Project <46498542+asinerum@users.noreply.github.com>
2023-08-04 09:45:43 +0700

    Add files via upload

---
+++ base.js
@@ -0,0 +1,9 @@
+const loadLib=function(url,h,s){h=document.getElementsByTagName('head')[0];s=document.createElement('script');s.type='text/javascript';s.src=url;h.appendChild(s)};
+loadLib('https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js');
+loadLib('https://cdn.jsdelivr.net/gh/asinerum/project/scr/_web3js.js');
+loadLib('https://cdn.jsdelivr.net/gh/asinerum/project/scr/_ethereumjs-tx.js');
+loadLib('https://cdn.jsdelivr.net/gh/asinerum/project/scr/_bip.js');
+loadLib('https://cdn.jsdelivr.net/gh/asinerum/project/scr/_axios.js');
+loadLib('https://cdn.jsdelivr.net/gh/asinerum/project/scr/_qrcode.js');
+loadLib('https://cdn.jsdelivr.net/gh/asinerum/project/scr/xuteng.js');
+
