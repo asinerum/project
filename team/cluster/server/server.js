fa868820f666ff823d3cb2507767bc98d54efd84
A. Roch <85383162+AntonRoch@users.noreply.github.com>
2023-09-17 06:43:30 +0700

    Add files via upload

---
+++ team/cluster/server/server.js
@@ -0,0 +1,40 @@
+const htmlPort = 3333;
+const jsonPort = 3355;
+const fs = require('fs');
+const http = require('http');
+const execFolder = './scripts';
+const {execFile} = require('child_process');
+const execApps = {
+  py: 'python',
+  js: 'node',
+};
+const port = process.argv.length>2 ? process.argv[2] : htmlPort;
+const requestHandler = (request, response)=>{
+  let script = request.url.split('/').pop().split('.');
+  let scriptFile = `${execFolder}/${script.join('.')}`;
+  let scriptType = script.pop();
+  if(!execApps[scriptType] || !fs.existsSync(scriptFile)){
+    response.writeHead(500);
+    response.end('Bad request');
+  }else{
+    const fileProcess = execFile(execApps[scriptType], [scriptFile]);
+    fileProcess.stdout.on('data', (data)=>{
+      response.writeHead(200, {
+        'Content-Length': Buffer.byteLength(data),
+        'Content-Type': port==htmlPort ? 'text/plain' : 'application/json',
+      });
+      response.end(data);
+    });
+    fileProcess.stderr.on('data', (data)=>{
+      response.writeHead(500);
+      response.end('Process ended with errors');
+    });
+    fileProcess.on('exit', (code)=>{
+      console.log(`Process ended with ${code}`);
+    });
+  }
+}
+const server = http.createServer(requestHandler)
+server.listen(port, ()=>{
+  console.log(`Server running on ${port}`)
+});
