3124b82686d87a73122fbdcab9f95166b633f1e7
Asinerum Conlang Project <46498542+asinerum@users.noreply.github.com>
2025-12-22 20:29:39 +0700

    Add files via upload

--- team/buas.js
+++ team/buas.js
@@ -3,7 +3,7 @@ const urlJsonData1 = 'https://cdn.jsdelivr.net/gh/asinerum/project/team/buas.jso
 const urlJsonData2 = 'https://asinerum.github.io/project/team/buas.json';
 
 const loadMembers = function(cbf=console.log, url=urlJsonData1){
-  var xhr = new XMLHttpRequest();
+  let xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
   xhr.responseType = 'json';
   xhr.onload = function(){
@@ -17,4 +17,20 @@ const loadMembers = function(cbf=console.log, url=urlJsonData1){
   xhr.send();
 }
 
-loadMembers(function(err, res){if(!err){members=res.members}else{console.error(err)}});
+const loadMembersSync = function(url=urlJsonData1){
+  let xhr = new XMLHttpRequest();
+  try{
+    xhr.open('GET', url, false);
+    xhr.send(null);
+    if(xhr.status===200){
+      console.log('Done');
+      return(JSON.parse(xhr.responseText));
+    }
+  }catch(e){
+    console.error(`ERROR: ${e}`);
+  }
+  return({});
+}
+
+// loadMembers(function(err, res){if(!err){members=res.members}else{console.error(err)}});
+members = loadMembersSync().members;
