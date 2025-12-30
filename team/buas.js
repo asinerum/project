206f9ecdb69e6ee84d874b28d9d2bfefda502cc6
A. Roch <85383162+AntonRoch@users.noreply.github.com>
2025-12-31 03:57:55 +0700

    Add files via upload

--- team/buas.js
+++ team/buas.js
@@ -1,41 +1,43 @@
-﻿let members;
+﻿// Web Version
+
+globalThis.members;
+
 const urlJsonData1 = 'https://cdn.jsdelivr.net/gh/asinerum/project/team/buas.json';
 const urlJsonData2 = 'https://asinerum.github.io/project/team/buas.json';
 
-const loadMembers = function(cbf=console.log, url=urlJsonData1){
+globalThis.loadMembers = function (cbf=console.log, url=urlJsonData1) {
   let xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
   xhr.responseType = 'json';
-  xhr.onload = function(){
+  xhr.onload = function () {
     var status = xhr.status;
-    if(status===200){
+    if (status === 200) {
       cbf(null, xhr.response);
-    }else{
+    } else {
       cbf(status, null);
     }
   }
   xhr.send();
 }
 
-const loadMembersSync = function(url=urlJsonData1){
+globalThis.loadMembersSync = function (url=urlJsonData1) {
   let xhr = new XMLHttpRequest();
-  try{
+  try {
     xhr.open('GET', url, false);
     xhr.send(null);
-    if(xhr.status===200){
-      console.log('Done');
-      return(JSON.parse(xhr.responseText));
+    if (xhr.status === 200) {
+      console.log('Team loaded');
+      return JSON.parse(xhr.responseText);
     }
-  }catch(e){
+  } catch(e) {
     console.error(`ERROR: ${e}`);
   }
-  return({});
+  return {};
 }
 
-// loadMembers(function(err, res){if(!err){members=res.members}else{console.error(err)}});
-
 members = loadMembersSync().members;
-numIndexVip1 = members.numIndexVip1;
-numIndexVip2 = members.numIndexVip2;
-UserVIPs = members.UserVIPs;
-UserBLs = members.UserBLs;
+
+globalThis.numIndexVip1 = members.numIndexVip1;
+globalThis.numIndexVip2 = members.numIndexVip2;
+globalThis.UserVIPs = members.UserVIPs;
+globalThis.UserBLs = members.UserBLs;
